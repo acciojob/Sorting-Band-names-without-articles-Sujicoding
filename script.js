88101f36f9027084ced38765b5caf1f4d124edfa
Sujicoding <117893004+Sujicoding@users.noreply.github.com>
2023-07-20 14:05:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-// script.js
-let bandNames = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-
+ let bandNames = ['The Rolling Stones', 'Aerosmith', 'Led Zeppelin', 'The Beatles'];
 function sortBandNames(names) {
-  const articles = ['a', 'an', 'the'];
-  return names.sort((a, b) => {
-    const name1 = a.toLowerCase().split(' ').filter(word => !articles.includes(word)).join(' ');
-    const name2 = b.toLowerCase().split(' ').filter(word => !articles.includes(word)).join(' ');
-    return name1.localeCompare(name2);
-  });
-}
+      const articles = ['a', 'an', 'the'];
+      return names.sort((a, b) => {
+        const name1 = a.toLowerCase().replace(/^(the|an|a)\s+/, '');
+        const name2 = b.toLowerCase().replace(/^(the|an|a)\s+/, '');
+        return name1.localeCompare(name2);
+      });
+    }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const sortedBandNames = sortBandNames(bandNames);
-  const bandList = document.getElementById('band');
+    document.addEventListener('DOMContentLoaded', () => {
+      const sortedBandNames = sortBandNames(bandNames);
+      const bandList = document.getElementById('band');
 
-  sortedBandNames.forEach(name => {
-    const li = document.createElement('li');
-    li.textContent = name;
-    bandList.appendChild(li);
-  });
-});
-
-
+      sortedBandNames.forEach(name => {
+        const li = document.createElement('li');
+        li.textContent = name;
+        bandList.appendChild(li);
+      });
+    });
