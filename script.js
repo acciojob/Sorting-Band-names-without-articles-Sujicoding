16362b6d39854c9ec0c8031c7e80319b7a2edc9b
Sujicoding <117893004+Sujicoding@users.noreply.github.com>
2023-07-20 14:01:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-//your code here
+// script.js
+let bandNames = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+
+function sortBandNames(names) {
+  const articles = ['a', 'an', 'the'];
+  return names.sort((a, b) => {
+    const name1 = a.toLowerCase().split(' ').filter(word => !articles.includes(word)).join(' ');
+    const name2 = b.toLowerCase().split(' ').filter(word => !articles.includes(word)).join(' ');
+    return name1.localeCompare(name2);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const sortedBandNames = sortBandNames(bandNames);
+  const bandList = document.getElementById('band');
+
+  sortedBandNames.forEach(name => {
+    const li = document.createElement('li');
+    li.textContent = name;
+    bandList.appendChild(li);
+  });
+});
+
 
